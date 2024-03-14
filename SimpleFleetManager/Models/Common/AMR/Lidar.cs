e3410099e0206f4f83b8f0f0931ff2bf95ba85b8
Wojtek <Wojtek@DHI-PC-WB>
2024-03-14 12:01:01 +0100

    Dodaj pliki projektów.

---
+++ SimpleFleetManager/Models/Common/AMR/Lidar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFleetManager.Models.Common.AMR
+{
+    public class Lidar
+    {
+        public bool IsActive { get; set; }
+        public bool ContaminationError { get; set; }
+        public bool ContaminationWarning { get; set; }
+        public bool DeviceError { get; set; }
+        public bool AppError { get; set; }
+        public bool SwitchMonitoringCaseError { get; set; }
+        public bool ReducedSpeedZoneStatus { get; set; }
+        public bool SoftStopZoneStatus { get; set; }
+        public bool emergencyStopZoneStatus { get; set; }
+    }
+}
